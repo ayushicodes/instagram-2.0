9019458b6b916d0705bd6054c8552ec26ab3e9cf
ayushicodes <raiayushi12911@gmail.com>
2021-11-30 21:33:47 +0530

    title changed

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
