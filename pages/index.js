ab676c899bf54fc918ae81a0ff671c997cea1059
ayushicodes <raiayushi12911@gmail.com>
2021-12-29 20:35:23 +0530

    header updated

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from 'next/head'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Instagram-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>this is instagram clone</h1>
+      {/* Header */}
+      <Header />
+      {/* feed */}
+      {/* Modal */}
+
     </div>
   )
 }
