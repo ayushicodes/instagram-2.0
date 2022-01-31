e4f04f97be672d02bf46fbb5ddd7f65990f9a6a7
ayushicodes <raiayushi12911@gmail.com>
2022-01-31 12:22:47 +0530

    Story Updated

--- pages/components/Story.js
+++ pages/components/Story.js
@@ -1,9 +1,9 @@
 function Story({ img, username }) {
     return (
         <div>
-            <img src={img} alt="" />
-            <p>{username}</p>
 
+            <img className="h-14 w-14 rounded-full p[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out" src="https://th.bing.com/th/id/OIP.ETldt3fKPP3GktQWp86PYQHaHa?pid=ImgDet&rs=1" alt="user image" />
+            <p className="text-xs w-14 truncate text-center "> {username}</p>
         </div>
     )
 }
